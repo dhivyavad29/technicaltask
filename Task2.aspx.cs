9b66c8e04546e98598d2dc519ca5c92e40b87618
dhivyavad29 <98062508+dhivyavad29@users.noreply.github.com>
2022-01-20 10:03:31 +1100

    Create Task2.aspx.cs

---
+++ Task2.aspx.cs
@@ -0,0 +1,27 @@
+Task: Find the bugs, why is it a bug and suggest a solution
+
+//duplicate records
+var duplicateRecords = dtDuplicateRows.Rows
+.Cast<DataRow>()
+.GroupBy(r => r["SERVICE_NUMBER"])
+.Where(g => g.Count() <3)
+.SelectMany(g => g).CopyToDataTable();
+splittedRecordsSet.Tables.Add(retrievedDataSet.Tables[0].Clone());
+//Add Action details
+if (duplicateRecords.Rows.Count > 1)
+{
+DataTable dtActionUpdated =new DataTable();
+var groups = duplicateRecords.Rows.Cast<DataRow>().GroupBy(x => x["SERVICE_NUMBER"]);
+foreach (var group in groups)
+{
+dtActionUpdated = (DuplicateRecordActionString(group.CopyToDataTable()));
+foreach (DataRow dr in dtActionUpdated.Rows)
+{
+splittedRecordsSet.Tables[1].ImportRow(dr);
+}
+Internal Use
+Internal Use
+}
+}
+splittedRecordsSet.Tables[1].TableName = "Duplicates";
+splittedRecordsSet.Tables[1].AcceptChanges();][
